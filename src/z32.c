e7d9da9c52e088726db6cdb99dbf84c2686dbd59
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-01-06 12:23:31 +0100

    Add UTF16 variants

--- src/z32.c
+++ src/z32.c
@@ -4,7 +4,13 @@
 #include "../include/z32.h"
 
 extern int
-z32_encode (const uint8_t *buffer, size_t buffer_len, utf8_t *string, size_t *string_len);
+z32_encode_utf8(const uint8_t *buffer, size_t buffer_len, utf8_t *string, size_t *string_len);
 
 extern int
-z32_decode (const utf8_t *string, size_t string_len, uint8_t *buffer, size_t *buffer_len);
+z32_decode_utf8(const utf8_t *string, size_t string_len, uint8_t *buffer, size_t *buffer_len);
+
+extern int
+z32_encode_utf16le(const uint8_t *buffer, size_t buffer_len, utf16_t *string, size_t *string_len);
+
+extern int
+z32_decode_utf16le(const utf16_t *string, size_t string_len, uint8_t *buffer, size_t *buffer_len);
--- test/decode.c
+++ test/decode.c
@@ -9,14 +9,14 @@
     size_t string_len = strlen(string); \
     size_t buffer_len = string_len; \
     uint8_t buffer[buffer_len]; \
-    int err = z32_decode((utf8_t *) string, string_len, buffer, &buffer_len); \
+    int err = z32_decode_utf8((utf8_t *) string, string_len, buffer, &buffer_len); \
     assert(err == 0); \
     assert(buffer_len == strlen(expected)); \
     assert(strncmp((char *) buffer, expected, buffer_len) == 0); \
   };
 
 int
-main () {
+main() {
   test_decode("cr", "a");
   test_decode("cfty", "ab");
   test_decode("cftgg", "abc");
